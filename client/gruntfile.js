d6a5a83463a57051066cd92473776051803e07a9
Prince Kumar <prince18pat@gmail.com>
2015-11-09 00:04:18 +0900

    Creating a server and initialising index file

--- client/gruntfile.js
+++ client/gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt){
-  // A very basic default task.
+  // Tasks are described here
   grunt.registerTask('default', 'Log some stuff.', function() {
     grunt.log.write('Logging some stuff...').ok();
   });
+
+  //Project configuration starts from here
 
 };
--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-//server
+var express = require('express');
+
+var app = express();
+
+
+app.get('/',function(req,res){
+
+  res.send('Hello World');
+
+});
+
+var server = app.listen(3000, function(){
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Server is now running at http://%s:%s",host,port);
+
+});
