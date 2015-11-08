7709b6b6e9be9eeba3e4d74869e62a3b131cc97f
Prince Kumar <prince18pat@gmail.com>
2015-11-09 02:22:40 +0900

    grunt file update

--- client/gruntfile.js
+++ client/gruntfile.js
@@ -1,9 +1,30 @@
 module.exports = function(grunt){
+
+  //Loading the grunt task library
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+
   // Tasks are described here
-  grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('Logging some stuff...').ok();
-  });
+  grunt.registerTask('default',['concat']);
 
   //Project configuration starts from here
+  grunt.initConfig({
+    //disribution directory
+    distdir: 'dist',
+    //src directory
+    src: {
+      js: ['src/**/*.js'],
+      html: ['src/index.html']
+    },
+    concat: {
+      index: {
+        src: ['src/index.html'],
+        dest: '<%= distdir %>/index.html',
+        options: {
+          process: true
+        }
+      }
+    }
+  });
 
 };
