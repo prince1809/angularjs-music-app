33b85a910df831a9099ef624528af60592c8a829
Prince Kumar <prince18pat@gmail.com>
2015-11-10 00:47:40 +0900

    Adding audio file

--- client/src/app.js
+++ client/src/app.js
@@ -6,3 +6,20 @@ app.controller('MyController',function($scope){
     name: 'Prince Kumar'
   }
 });
+
+app.controller('PlayerController',function($scope){
+
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src = "http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61";
+  $scope.play = function(){
+    $scope.audio.play();
+    $scope.playing = true;
+  }
+
+  $scope.stop = function(){
+    $scope.audio.pause();
+    $scope.playing = false;
+  }
+
+});
